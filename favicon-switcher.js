37e5a49ce65b0a734f61fc53bb6d0d2276bfc38d
itzhamstertime <belfif50@gmail.com>
2025-08-06 20:10:15 +0200

    Update favicon-switcher.js

--- favicon-switcher.js
+++ favicon-switcher.js
@@ -1,1 +1,12 @@
+const normal = 'https://i.imgur.com/dNiFXNN.png';
+const blink = 'https://i.imgur.com/xen8G5e.png';
+const favicon = document.getElementById('favicon');
+
+setInterval(() => {
+  if (favicon.href.endsWith(normal)) {
+    favicon.href = blink;
+  } else {
+    favicon.href = normal;
+  }
+}, 20000);
 
