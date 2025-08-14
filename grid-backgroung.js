58917014e85cd14e37093b4f81873eea84852fcc
itzhamstertime <belfif50@gmail.com>
2025-08-14 05:13:24 +0200

    Create grid-backgroung.js

---
+++ grid-backgroung.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // existing music-player code...
+
+    function setGridBackground() {
+        document.body.style.backgroundImage = "url('https://i.imgur.com/YOUR_IMAGE.png')";
+        document.body.style.backgroundRepeat = "repeat";
+        document.body.style.backgroundSize = "32px 32px";
+        document.body.style.backgroundColor = "cyan";
+    }
+
+    setGridBackground(); // call it once the DOM is ready
+});
